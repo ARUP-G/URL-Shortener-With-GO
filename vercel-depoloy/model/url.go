dfeff981dba4ecb9493f3845261dabb0aa297a33
Arup <darup2019@gmail.com>
2024-09-21 12:53:22 +0530

    vercel-deployment deleted

--- vercel-depoloy/model/url.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type URL struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty"`
-	LongURL  string             `bson:"long_url"`
-	ShortURL string             `bson:"short_url"`
-}
